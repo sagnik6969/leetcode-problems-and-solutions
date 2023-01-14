55164ee0e01833d68afd8aa02726b53ad70bd3eb
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2023-01-14 11:56:16 +0530

    Time: 41 ms (80.71%), Space: 89.3 MB (90.48%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -15,8 +15,7 @@ public:
             
             int cut1 = (low + high) / 2; 
             int cut2 = size - cut1;
-           // cout << cut1 << " " << cut2 << endl;
-            
+           
             int l1 = cut1 - 1 < 0 ? INT_MIN : nums1[cut1 - 1];
             int r1 = cut1 >= m ? INT_MAX : nums1[cut1];
             int l2 = cut2 - 1 < 0 ? INT_MIN : nums2[cut2 - 1];
@@ -25,12 +24,8 @@ public:
             if(l1 <= r2 && l2 <= r1){
                 
                 if((m + n) % 2 != 0) return max(l1,l2);
-                
-              //  cout << "a";
-                
+              
                 return (max(l1,l2) + min(r1,r2))/2.0;
-                
-                
             }
             else if(r2 < l1) high = cut1 - 1;
             else low = cut1 + 1;
