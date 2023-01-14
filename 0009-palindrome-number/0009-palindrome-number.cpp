49c19d1172a9040f8151821571434393b5207c2b
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2023-01-14 19:05:39 +0530

    Time: 15 ms (77.17%), Space: 6 MB (12.85%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        if(x == 0) return true;
+        if(x < 0) return false;
+        
+        string s = "";
+        
+        while(x != 0){
+            
+            s.push_back('0' + x % 10);
+            
+            x /= 10;
+            
+        }
+        
+        int i = 0;
+        int j = s.size() - 1;
+        
+        while(i < j){
+            
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+            
+        }
+        
+        return true;
+        
+    }
+};
