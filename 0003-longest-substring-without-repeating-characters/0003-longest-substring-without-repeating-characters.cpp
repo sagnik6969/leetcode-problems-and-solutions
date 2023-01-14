9a47712c2b6aeb54eac7b13734539d10c596d78e
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2023-01-14 10:32:20 +0530

    Time: 25 ms (60.16%), Space: 8.3 MB (62.46%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,27 +5,20 @@ public:
         unordered_map<char,int> cnt;
         
         int i = 0;
-        int j = 0;
-        
+        int j = 0; 
         int ans = 0;
         
         while(j < s.size()){
             
             cnt[s[j]]++;
-            
             int ch = s[j];
-            
             j++;
             
             while(cnt[ch] > 1){
-                
                 cnt[s[i]]--;
                 i++;
-            
             }
-            
             ans = max(ans,j - i);
-            
         }
         
         return ans;
