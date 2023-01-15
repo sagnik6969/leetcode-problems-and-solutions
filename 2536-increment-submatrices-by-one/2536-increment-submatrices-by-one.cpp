68551561c02468ad1019863d57a70366d62ac236
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2023-01-15 10:30:31 +0530

    Time: 341 ms (80.00%), Space: 82 MB (40.00%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -10,30 +10,19 @@ public:
             
             int r1 = i[0];
             int c1 = i[1];
-            
             int r2 = i[2];
             int c2 = i[3];
-            
             for(int x = r1 ; x <= r2; x++){
-                
                 mat[x][c1]++;
-                
                 if(c2 + 1 < n) mat[x][c2+1]--;
-                
             }
-            
         }
-        
         
         for(int i = 0 ; i < n ; i++){
             for(int j = 1 ; j < n ; j++){
                 mat[i][j] += mat[i][j-1];
             }
-        }
-        
-        
-    
-        
+        }    
         return mat;
     }
     
