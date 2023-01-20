d56e034cc4b43c92522481f90349b99307e49008
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2023-01-20 08:37:41 +0530

    Time: 105 ms (48.11%), Space: 45.1 MB (23.81%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    void rec(int idx,vector<int> &arr,vector<int> &curr,set<vector<int>> &ans){
+        
+        if(idx == arr.size()){
+            if(curr.size() > 1)
+                ans.insert(curr);
+            return;
+        }
+        
+        rec(idx+1,arr,curr,ans);
+        
+        if(curr.empty() || curr.back() <= arr[idx]){
+            curr.push_back(arr[idx]);
+            rec(idx+1,arr,curr,ans);
+            curr.pop_back();
+        }
+        
+    }
+    
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        
+        vector<int> curr;
+        set<vector<int>> ans;
+        
+        rec(0,nums,curr,ans);
+        
+        vector<vector<int>> final_ans;
+        
+        for(auto &i:ans) final_ans.push_back(i);
+        
+        return final_ans;
+        
+    }
+};
