1366ef3259e8ff97f5fb76a64a23e0584d4aa03b
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2023-01-27 13:40:56 +0530

    Time: 4 ms (62.51%), Space: 6.5 MB (91.08%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    
+    char better_approach(string &s,string &t){
+        
+        char ans = 0;
+        for(auto i:s) ans ^= i;
+        for(auto i:t) ans ^= i;
+        
+        return ans;
+        
+    }
+    
     char findTheDifference(string s, string t) {
         
       int arr[26] = {0};
