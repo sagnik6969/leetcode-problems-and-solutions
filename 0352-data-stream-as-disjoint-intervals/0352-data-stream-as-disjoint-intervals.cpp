913a7a31b3dbf02f4710391c19bf38e8e1eeb438
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2023-01-28 10:22:14 +0530

    Time: 55 ms (93.73%), Space: 34 MB (24.31%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -1,52 +1,30 @@
 class SummaryRanges {
 public:
-    
-  
     map<int,int> begin_to_end;
     map<int,int> end_to_begin;
     set<int> s;
-    
-    SummaryRanges() {
-        
-    }
-    
+    SummaryRanges() {}
     void addNum(int value) {
-
         if(s.find(value) == s.end()) s.insert(value);
         else return; 
-       
         int begin = value;
         int end = value;
-        
         if(begin_to_end.find(value+1) != begin_to_end.end()){
             end = begin_to_end[value+1];
             end_to_begin.erase(end);
             begin_to_end.erase(value+1); 
         }
-        
         if(end_to_begin.find(value-1) != end_to_begin.end()){
             begin = end_to_begin[value-1];
             begin_to_end.erase(begin);
             end_to_begin.erase(value-1);
         }
-        
         begin_to_end[begin] = end;
         end_to_begin[end] = begin;
-        
     }
-    
     vector<vector<int>> getIntervals() {
-        vector<vector<int>> v;
-        
+        vector<vector<int>> v;   
         for(auto i:begin_to_end) v.push_back({i.first,i.second});
-        
         return v;
     }
 };
-
-/**
- * Your SummaryRanges object will be instantiated and called as such:
- * SummaryRanges* obj = new SummaryRanges();
- * obj->addNum(value);
- * vector<vector<int>> param_2 = obj->getIntervals();
- */
