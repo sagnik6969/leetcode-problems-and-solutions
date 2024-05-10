0add27b6152ea875aebcedcbd421aec6730b9ef0
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-05-10 08:43:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,23 +1,33 @@
+vector<int> v;
+
+class compare {
+    public:
+    bool operator()(pair<int,int> &p1,pair<int,int> &p2){
+        return v[p1.first] * v[p2.second] > v[p2.first] * v[p1.second];
+    }
+};
+
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+     v = arr;
 
-        vector<pair<int,int>> v;
-        v.reserve(arr.size());
+     priority_queue<pair<int,int>,vector<pair<int,int>>,compare> q;
 
-        for(int i = 0 ; i < arr.size() ; i++){
-            for(int j = i + 1 ; j < arr.size() ; j++){
-                v.push_back({arr[i],arr[j]});
-            }
-        }
+     for(int i = 0 ; i < arr.size()-1; i++) q.push({i,arr.size()-1});
 
-        sort(v.begin(),v.end(),[&](pair<int,int> &p1,pair<int,int> &p2){
-            return p1.first * p2.second < p2.first * p1.second;
-        });
+     for(int i = 0 ; i < k-1 ; i++){
+        int x = q.top().first;
+        int y = q.top().second;
+        cout << v[x] << " " << v[y] << endl;
+        q.pop();
+        y--;
 
-        return {v[k-1].first,v[k-1].second};
+        if(y != x) q.push({x,y});
 
+     }
 
+      return {v[q.top().first],v[q.top().second]};
         
     }
 };
