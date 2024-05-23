b2abee9ba25c5487240a56cc478709b927acbc5c
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-05-23 12:57:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    bool is_valid(int x){
-        return x >= 0 && x <= 1000;
-    }
-    void f(vector<int>& nums, int k,vector<int> &curr,int idx,int &ans){
-        if(idx == nums.size()){
-             ans++;
-             return;
+    int f(vector<int>& nums, int k,multiset<int> &curr,int idx){
+        if(idx == nums.size()) return curr.empty() ? 0:1;
+        
+        int ans = f(nums,k,curr,idx+1);
+
+        if(curr.find(nums[idx] - k) == curr.end() && curr.find(nums[idx] + k) == curr.end()){
+            curr.insert(nums[idx]);
+            ans += f(nums,k,curr,idx+1);
+            curr.erase(curr.find(nums[idx]));
         }
         
-        f(nums,k,curr,idx+1,ans);
-
-        if((!is_valid(nums[idx]-k) || curr[nums[idx] - k] == 0)
-        &&
-        (!is_valid(nums[idx]+k) || curr[nums[idx]+k] == 0)){
-            curr[nums[idx]]++;
-            f(nums,k,curr,idx+1,ans);
-            curr[nums[idx]]--;
-        }
+        return ans;
 
     }
     int beautifulSubsets(vector<int>& nums, int k) {
 
-        vector<int> curr(1001,0);
-        int ans = 0;
+        multiset<int> curr;
 
-         f(nums,k,curr,0,ans);
-        return ans - 1;
+        return f(nums,k,curr,0);
+        
     }
 };
